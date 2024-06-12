b7322f37fe3aaec19466c95fb8538ac5874898a3
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-06-12 14:29:52 +0900

    絞り込み

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT || 3001;
 
 app.post("/", async (req, res) => {
+  const fullUrl = req.protocol + "://" + req.headers.host + req.url;
+  console.log(fullUrl);
+  if (fullUrl !== "https://nippo-kun.vercel.app/") {
+    res.json({ message: "Hello World!" });
+    return;
+  }
+
   const genAI = new GoogleGenerativeAI(process.env.API_KEY);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
   const prompt = `${req.body.value} 上記文章は日報です。これについて${req.body.order}の観点からレビューしてください。また太字のマークダウンの代わりに見出しや箇条書きを使って書いてください。改善点があれば具体的に記述してください`;
@@ -30,6 +37,12 @@ app.post("/", async (req, res) => {
 });
 
 app.post("/all-review", async (req, res) => {
+  const fullUrl = req.protocol + "://" + req.headers.host + req.url;
+  if (fullUrl !== "https://nippo-kun.vercel.app/") {
+    res.json({ message: "Hello World!" });
+    return;
+  }
+
   const genAI = new GoogleGenerativeAI(process.env.API_KEY);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
   const prompt = `${req.body.value} 上記文章は日報です。誤字脱字があれば校閲してください。日報の内容についてモチベーションが上がるように誉めてください。また太字のマークダウンの代わりに見出しや箇条書きを使って書いてください。改善点があれば具体的に記述してください`;
